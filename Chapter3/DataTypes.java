efe84aba30fa38d3debdd1eb6db450294af51b6b
Anupam Sinha <57113053+asinhacode@users.noreply.github.com>
2020-02-12 19:23:17 -0500

    Add files via upload

---
+++ Chapter3/DataTypes.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class DataTypes
+{
+    public static void main(String[] args)
+    {
+        /*
+        byte a = -128;
+
+        for(int i = 0; i != 257; i++)
+        {
+            System.out.println(a);
+            a++;
+        }
+        */
+
+        Netflix anupam = new Netflix("Anupam Sinha", "Basic");
+        Netflix gabriel = new Netflix("Gabriel Feliciano", "Premium");
+        Netflix nabamita = new Netflix("Nabamita Sinha", "Premium");
+
+        Netflix friends[] = {anupam, gabriel, nabamita};
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Chapter3/Netflix.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Netflix {
+
+    String accountName;
+    String plan;
+
+    public Netflix(String aN, String p)
+    {
+        accountName = aN;
+        plan = p;
+    }
+}
