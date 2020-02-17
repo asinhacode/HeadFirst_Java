2b0d0eba67237ce41d92bd3d556f44005e7120ab
Anupam Sinha <57113053+asinhacode@users.noreply.github.com>
2020-02-16 23:16:55 -0500

    Create Test.java

---
+++ ch_6/Test.java
@@ -0,0 +1,11 @@
+package com.company.inheritance;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Publisher Sinha = new Publisher("Sinha", true, "Dixon Congregation");
+        Sinha.currentInfo();
+    }
+
+}
